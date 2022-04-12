654178aa168c4f8543a19de63995396fd0a5f124
Mohamed NAYAZ <mohamed.n.tech@gmail.com>
2022-04-12 12:48:13 +0530

    My First commit on the Java Project

---
+++ src/main/java/com/example/demoHelloWorld/DemoHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHelloWorldApplication.class, args);
+		System.out.println("Hello There i am sample project");
+	}
+
+}
